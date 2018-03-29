3da5aedb48b9293094744c5200f46bb811e03538
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-03-29 03:23:24 -0500

    added math to determine next arrival time and minutes remaining until next arrival

--- assets/js/trainTimes.js
+++ assets/js/trainTimes.js
@@ -18,10 +18,10 @@ let trainName;
 let trainDestination;
 let trainFirstTime;
 let trainFrequency;
+let nextTrainTimeInMinutes;
+let nextTrainTime;
 
 $(document).ready(function() {
-
-   
 
     // add firebase to DOM if there's something to show every time there's a value for a new child
     database.ref().on('value', function(snapshot) {
@@ -34,15 +34,48 @@ $(document).ready(function() {
             trainName = value.name;
             trainDestination = value.destination;
             trainFirstTime = value.first_time;
-            trainFrequency = value.frequency;
+            trainFrequency = parseInt(value.frequency);
+
+            //convert first time to minutes
+            let trainHoursToMinutes = parseInt(moment(trainFirstTime, 'HH:mm').format('HH') * 60);
+            let trainRemainderMinutes = parseInt(moment(trainFirstTime, 'HH:mm').format('mm'));
+            let trainTimeInMinutes = trainHoursToMinutes + trainRemainderMinutes;
+            console.log(trainFirstTime, trainTimeInMinutes);
+
+            //convert current time to minutes
+            let currentHoursToMinutes = parseInt(moment().format('HH') * 60);
+            let currentRemainderMinutes = parseInt(moment().format('mm'));
+            let currentTimeInMinutes = currentHoursToMinutes + currentRemainderMinutes;
+            console.log(currentTimeInMinutes, trainFrequency);
+
+            //Loop through time adding duration until it is greater than current time
+
+            if (currentTimeInMinutes > trainTimeInMinutes) {
+                for (let i = trainTimeInMinutes; i < currentTimeInMinutes; i = i + trainFrequency) {
+                    nextTrainTimeInMinutes = i + trainFrequency;
+                    // console.log(i);
+                }
+                // nextTrainTimeInMinutes = moment(nextTrainTimeInMinutes, 'mm').format('HH:mm');
+                // console.log(nextTrainTimeInMinutes);
+                nextTrainTimeInMinutes = nextTrainTimeInMinutes - currentTimeInMinutes;
+
+            } else {
+                nextTrainTimeInMinutes = trainTimeInMinutes - currentTimeInMinutes;
+                // console.log(nextTrainTimeInMinutes);
+            }
+
+            nextTrainTime = getTimeFromMins(nextTrainTimeInMinutes + currentTimeInMinutes);   
+
+
+            //Then get the difference between that new time and current time (in minutes)
 
 
             let tableRow = $('<tr>');
             tableRow.append(`<td>${trainName}</td>`);
             tableRow.append(`<td>${trainDestination}</td>`);
             tableRow.append(`<td>${trainFrequency}</td>`);
-            tableRow.append(`<td>n/a</td>`);
-            tableRow.append(`<td>n/a</td>`);
+            tableRow.append(`<td>${nextTrainTime}</td>`);
+            tableRow.append(`<td>${nextTrainTimeInMinutes}</td>`);
             $('#trainTimes > tbody').append(tableRow);
 
             // console.log(trainName, trainDestination, trainFirstTime, trainFrequency)
@@ -68,6 +101,9 @@ $(document).on('click', '#addTrain', function() {
     trainFirstTime = $('#firstTrainTime').val().trim();
     trainFrequency = $('#frequency').val().trim();
 
+
+
+
     // create object for this submitted train
     const pushObject = {
         name : trainName,
@@ -82,3 +118,12 @@ $(document).on('click', '#addTrain', function() {
 
 
 });
+
+
+
+function getTimeFromMins(mins) {
+
+    let h = mins / 60 | 0;
+    let m = mins % 60 | 0;
+    return moment.utc().hours(h).minutes(m).format("HH:mm");
+}
