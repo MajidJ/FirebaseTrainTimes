731c3da25fb4c5570b43da2f1857362a83aa313a
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-03-28 20:01:03 -0500

    added file structure, bootstrap scaffolding, initialized firebase

---
+++ assets/js/trainTimes.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+// Global Variables
+
+
+
+$(document).ready(function() {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAZNpta-SayyWiFtK-nDISWXuTVQVuIs-o",
+        authDomain: "employeedatamanage-340b6.firebaseapp.com",
+        databaseURL: "https://employeedatamanage-340b6.firebaseio.com",
+        projectId: "employeedatamanage-340b6",
+        storageBucket: "employeedatamanage-340b6.appspot.com",
+        messagingSenderId: "322499376194"
+    };
+    firebase.initializeApp(config);
+
+    // add firebase to DOM if there's something to show every time there's a value for a new child
+
+
+
+
+
+
+});
+
+
+$(document).on('click', '#addTrain', function() {
+    event.preventDefault();
+    // console.log('the button got clicked yo!');
+
+    // setup input variables 
+
+    // pass new submitted values
+
+    // create object for this submitted train
+
+
+    // pass values to firebase
+
+
+
+});
